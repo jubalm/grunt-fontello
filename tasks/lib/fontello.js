21e817c9fc2b1ffcb1296d06054790399e5559a9
Pawel Kowalewski <pawel@przesylarka.pl>
2016-03-16 08:40:31 +0100

    Change space variable from '\t' to '  '

--- tasks/lib/fontello.js
+++ tasks/lib/fontello.js
@@ -38,7 +38,7 @@ var setSession = function(options, session, config){
   // Write session to config file. Save session in name field since the
   // Fontello api dislikes custom members.
   config.name = session;
-  fs.writeFileSync(dest, JSON.stringify(config, null, '\t'));
+  fs.writeFileSync(dest, JSON.stringify(config, null, '  '));
 }
 
 /*
